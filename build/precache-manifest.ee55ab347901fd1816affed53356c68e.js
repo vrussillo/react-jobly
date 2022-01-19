7d66752ee6cd9e5d628e437e2ed9d48c8f7b46b9
vrussillo <80196694+vrussillo@users.noreply.github.com>
2022-01-18 22:51:48 -0800

    heroku repos

---
+++ build/precache-manifest.ee55ab347901fd1816affed53356c68e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2ecf374e8df05b75774e23027caa096",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7eff40a7fc8f8c947d05",
+    "url": "/static/css/2.5aa997f2.chunk.css"
+  },
+  {
+    "revision": "0bb890b5728ce342f015",
+    "url": "/static/css/main.658eb3e8.chunk.css"
+  },
+  {
+    "revision": "7eff40a7fc8f8c947d05",
+    "url": "/static/js/2.51a68d8c.chunk.js"
+  },
+  {
+    "revision": "0bb890b5728ce342f015",
+    "url": "/static/js/main.55e1d40c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5fff5b8e277c27768f74f4f246bffcfd.js"
+  "/precache-manifest.ee55ab347901fd1816affed53356c68e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
